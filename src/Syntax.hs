d26fe5b7b6a0c65dce5131e8b4c4085c7c5ba529
kadu-v <kazuonsunday@yahoo.co.jp>
2020-09-22 10:42:05 +0900

    instantiate LispVal for Show class

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,5 +1,6 @@
 module Syntax where
 
+--
 data LispVal
   = Atom String
   | List [LispVal]
@@ -7,4 +8,22 @@ data LispVal
   | Number Integer
   | String String
   | Bool Bool
-  deriving (Eq, Show)
+  deriving (Eq)
+
+--
+instance Show LispVal where
+  show = showVal
+
+--
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " ." ++ showVal tail ++ ")"
+
+--
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
