bcb8d58b24706497ef1fdf0e8d83621253916a02
kadu-v <kazuonsunday@yahoo.co.jp>
2020-09-26 20:20:32 +0900

    add primitive operator

--- test/EvalSpec.hs
+++ test/EvalSpec.hs
@@ -9,7 +9,16 @@ evalAndParse = eval . readExpr
 
 spec :: Spec
 spec = do
-  describe "test of eval" $ do
+  describe "simple test of eval" $ do
     it "eval \"hoge\"" $ evalAndParse "\"hoge\"" `shouldBe` (String "hoge")
     it "eval 123" $ evalAndParse "123" `shouldBe` (Number 123)
     it "eval #t" $ evalAndParse "#t" `shouldBe` (Bool True)
+
+  describe "primitive operator test of eval" $ do
+    it "operator: +" $ evalAndParse "(+ 1 1)" `shouldBe` (Number 2)
+    it "operator: -" $ evalAndParse "(- 2 3)" `shouldBe` (Number (-1))
+    it "operator: *" $ evalAndParse "(* 1 2)" `shouldBe` (Number 2)
+    it "operator: /" $ evalAndParse "(/ 1 1)" `shouldBe` (Number 1)
+    it "operator: mod" $ evalAndParse "(mod 5 3)" `shouldBe` (Number 2)
+    it "operator: quotient" $ evalAndParse "(quotient 5 3)" `shouldBe` (Number 1)
+    it "operator: remainder" $ evalAndParse "(remainder 3 7)" `shouldBe` (Number 3)
